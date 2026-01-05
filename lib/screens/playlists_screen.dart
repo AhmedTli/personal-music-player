bc96a2921fb96746335e5c90d3de1e19edd9f4d6
Ahmed Tlili <ahmed.14.tlili@gmail.com>
2026-01-05 11:55:58 +0100

    navigation and routing

---
+++ lib/screens/playlists_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class PlaylistsScreen extends StatelessWidget {
+  const PlaylistsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Music Player'),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(color: Colors.blueGrey),
+              child: Text("Menu"),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+              onTap: () {
+                Navigator.pushNamed(context, '/');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.favorite),
+              title: Text('Favorites'),
+              onTap: () {
+                Navigator.pushNamed(context, '/favorites');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.playlist_play),
+              title: Text('PlayLists'),
+              onTap: () {
+                Navigator.pushNamed(context, '/playlists');
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Center(child: Text('This is the Playlists Screen!')),
+    );
+  }
+}
